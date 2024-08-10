7a3cf77679f3223dc866c142b12a662132d179c2
Ilya Bylich <ibylich@gmail.com>
2024-08-10 14:54:36 +0200

    remove files related to debian packaging

--- build-in-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eux
-
-dh binary
-
-cp /build/waybar-network-applet_${BUILD_VERSION}_amd64.deb /shared
-cp /build/waybar-network-applet-$BUILD_VERSION/waybar-network-applet /shared
